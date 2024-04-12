6e7c403daba4a31fd0754d891b7f931634d5fe0c
Manri24 <157128196+Manri24@users.noreply.github.com>
2024-04-12 08:18:25 -0600

    Add files via upload

---
+++ Laboratorio/Laboratorio/Semana 13/L10+_+DAMR+1197624.py
@@ -0,0 +1,34 @@
+# numero = int(input("Ingrese un número: "))
+
+# print("Tabla de multiplicar del", numero)
+# for i in range(1, 11): 
+
+#    print(numero, "x", i, "=", numero * i)
+
+#Ejercicio 2 
+# numero = int(input("Ingresa un número para verificar si es primo: "))
+# es_primo = True
+
+# if numero <= 1:
+#    es_primo = False
+#else:
+#    divisor = 2
+#    while divisor < numero:
+#        if numero % divisor == 0:
+#            es_primo = False
+#            break
+#        divisor += 1
+
+# if es_primo:
+#    print(numero, "es un número primo.")
+# else:
+#    print(numero, "no es un número primo.")   
+
+#Ejercicio 3 
+#numero = int(input("Ingresa un número: "))
+#suma = 0
+
+#for i in range(1, numero + 1):
+#    suma += i
+
+#print("La suma desde 1 hasta", numero, "es:", suma)
